3cf4069b2adb4fd53dad88111151861d417a9568
Co-vengers <vyomrohila@gmail.com>
2025-03-19 12:05:42 +0530

    models updated

--- video_transcriber/transcription/models.py
+++ video_transcriber/transcription/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Video(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Link videos to users
     title = models.CharField(max_length=255)
     file = models.FileField(upload_to='videos/')
     transcript = models.TextField(blank=True, null=True)
