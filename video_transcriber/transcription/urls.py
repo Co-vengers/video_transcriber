c05cadb263c1a505c862a0f65fda72258b2b83ef
Co-vengers <vyomrohila@gmail.com>
2025-03-19 12:07:59 +0530

    transcription urls updated

--- video_transcriber/transcription/urls.py
+++ video_transcriber/transcription/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import upload_video, video_list, video_detail
+from .views import register, user_login, user_logout, upload_video, video_list, video_detail
 
 urlpatterns = [
     path('', upload_video, name='upload_video'),
     path('videos/', video_list, name='video_list'),
-    path('videos/<int:video_id>/', video_detail, name='video_detail'),  # New detail view
+    path('videos/<int:video_id>/', video_detail, name='video_detail'),
+
+    # Authentication URLs
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
 ]
