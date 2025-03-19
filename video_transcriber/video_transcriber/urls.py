c781b83039c05085fc3165c6d24cbd98973f12a7
Co-vengers <vyomrohila@gmail.com>
2025-03-19 19:18:08 +0530

    default path updated

--- video_transcriber/video_transcriber/urls.py
+++ video_transcriber/video_transcriber/urls.py
@@ -17,10 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import RedirectView
+# from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", RedirectView.as_view(url="/login/", permanent=False)),
+    # path("", RedirectView.as_view(url="/login/", permanent=False)),
     path("", include("transcription.urls")),
 ]
