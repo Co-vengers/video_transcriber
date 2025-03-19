080bf914210a98304bd5e1aae5b5952549533a0d
Co-vengers <vyomrohila@gmail.com>
2025-03-19 09:52:10 +0530

    views added

--- video_transcriber/transcription/views.py
+++ video_transcriber/transcription/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import VideoUploadForm
+from .models import Video
+from .utils import transcribe_video
 
-# Create your views here.
+def upload_video(request):
+    if request.method == 'POST':
+        form = VideoUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            video = form.save()
+            video.transcript = transcribe_video(video.file.path)
+            video.save()
+            return redirect('video_list')
+    else:
+        form = VideoUploadForm()
+    
+    return render(request, 'upload.html', {'form': form})
+
+def video_list(request):
+    videos = Video.objects.all()
+    return render(request, 'video_list.html', {'videos': videos})
